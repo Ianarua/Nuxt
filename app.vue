7603b2826d735222140824e4e6bd0040078bf263
Ianarua <3067815232@qq.com>
2024-01-06 18:13:08 +0800

    vant

--- app.vue
+++ app.vue
@@ -2,6 +2,17 @@
     <NuxtLink to="/">首页</NuxtLink>
     <br>
     <NuxtLink to="/video">视频页</NuxtLink>
-
+    <van-button type="primary">主要按钮</van-button>
     <NuxtPage/>
 </template>
+<script setup lang="ts">
+useSeoMeta({
+    title: 'bilibili',
+    description: '视频网站',
+});
+</script>
+<style>
+:root {
+    --van-primary-color: #FB7299 !important;
+}
+</style>
